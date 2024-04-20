350bb25eaccabf6edbc3bbb1b33ae99fe4d84bf0
gabrielapersonal <gabrielanasoli15@gmail.com>
2024-04-19 20:08:51 -0400

    adding database methods CRUD

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,18 +6,8 @@ import ReactDOM from 'react-dom/client';
 import HomePage from './HomePage';
 
 function LoginComponent() {
-    const [backendPhrase, setBackendPhrase] = useState('');
-
-    useEffect(() => {
-      axios.get('http://localhost:8080')
-        .then(response => {
-          console.log(response.data)
-          setBackendPhrase(response.data);
-        })
-        .catch(error => {
-          setBackendPhrase("oh it's not working, baby")
-        });
-    }, []);;
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const goToHome = () => {
         const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -28,25 +18,60 @@ function LoginComponent() {
         const root = ReactDOM.createRoot(document.getElementById('root'));
         root.render(<CartComponent />);
     }
+
+    const handleLogin = () => {
+        console.log("Username:", username);
+        console.log("Password:", password);
+        alert("Welcome " + {username});
+    };
         
     return (
-        <div class="homepage">
-                <nav class="navbar">
-                    <div class="logo"><h1>Gabi Fashion Store</h1></div>
-                    <ul class="menu">
-                        <li><a onClick={() => goToHome()}>Home</a></li>
-                        <li><a class="active">Login</a></li>
-                        <li><a onClick={() => goToCart()}>Cart</a></li>
-                    </ul>
-                    <div class="menu-btn">
-                        <i class="fa fa-bars"></i>
-                    </div>
-                </nav>
+        <div className="homepage">
+            <nav className="navbar">
+                <div className="logo"><h1>Gabi Fashion Store</h1></div>
+                <ul className="menu">
+                    <li><a onClick={() => goToHome()}>Home</a></li>
+                    <li><a className="active">Login</a></li>
+                    <li><a onClick={() => goToCart()}>Cart</a></li>
+                </ul>
+                <div className="menu-btn">
+                    <i className="fa fa-bars"></i>
+                </div>
+            </nav>
 
-                <footer>
-                <p>15 Wellignton Street Kitchener, CA - (226)899-XXXX</p>
-                <p class="copyright-text">© Gabriela Nascimento Oliveira Pereira</p>
-                </footer>
+            <section className="content">
+                <div className="mainContainer">
+                    <form onSubmit={handleLogin}>
+                        <h1>Login</h1><br/>
+                        <div className="inputContainer">
+                            <input 
+                                type="text" 
+                                placeholder="Enter Username"
+                                id="username" 
+                                value={username} 
+                                onChange={(e) => setUsername(e.target.value)} 
+                                required
+                            />
+                        </div> <br/>
+                        <div className="inputContainer">
+                            <input 
+                                type="password"
+                                placeholder="Enter Password" 
+                                id="password" 
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required 
+                            />
+                        </div>
+                        <button type="submit">Login</button> 
+                    </form>
+                </div>
+            </section>
+
+            <footer>
+                <p>15 Wellington Street Kitchener, CA - (226)899-XXXX</p>
+                <p className="copyright-text">© Gabriela Nascimento Oliveira Pereira</p>
+            </footer>
         </div>
     );
 }
